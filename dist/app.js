3cfacf9204b44e4b9eab7a2e3aab0fc8e08d0aab
Zosia <zosiagrafika11@gmail.com>
2023-04-24 23:22:46 +0200

    functionality/burgerMenu

---
+++ dist/app.js
@@ -0,0 +1,23 @@
+const burgerMenu = document.querySelector(`.burgerMenu`);
+const closeBurgerMenu = document.querySelector(`.cross`);
+const menuResponsive = document.querySelector(`.menuResponsive`);
+
+burgerMenu.addEventListener(`click`, () => {
+    burgerMenu.classList.add(`hidden`);
+    closeBurgerMenu.classList.remove(`hidden`);
+    menuResponsive.classList.remove(`hidden`);
+})
+
+closeBurgerMenu.addEventListener(`click`, () => {
+    burgerMenu.classList.remove(`hidden`);
+    closeBurgerMenu.classList.add(`hidden`);
+    menuResponsive.classList.add(`hidden`);
+})
+
+document.addEventListener('click', (event) => {
+    if (!closeBurgerMenu.contains(event.target) && !menuResponsive.contains(event.target) && !burgerMenu.contains(event.target)) {
+        burgerMenu.classList.remove(`hidden`);
+        closeBurgerMenu.classList.add(`hidden`);
+        menuResponsive.classList.add(`hidden`);
+    }
+})
